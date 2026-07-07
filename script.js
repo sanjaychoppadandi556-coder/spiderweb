7247cf31eb37fa7580cdf1eeca989330499c5148
sanjaychoppadandi556-coder <sanjaychoppadandi556@gmail.com>
2026-07-07 02:14:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
-const hero = document.getElementById("spiderman");
-const rope = document.getElementById("rope");
+const hero = document.querySelector(".spider");
+const rope = document.querySelector(".web-line");
 
 let angle = -45;
 let speed = 1.2;
 let direction = 1;
 
-function animate(){
+function animate() {
+  angle += speed * direction;
 
-    angle += speed * direction;
+  if (angle > 45) direction = -1;
+  if (angle < -45) direction = 1;
 
-    if(angle>45){
-        direction=-1;
-    }
+  hero.style.transform = `rotate(${angle}deg)`;
+  rope.style.transform = `rotate(${angle}deg)`;
 
-    if(angle<-45){
-        direction=1;
-    }
-
-    hero.style.transform =
-        `rotate(${angle}deg)`;
-
-    rope.style.transform =
-        `rotate(${angle}deg)`;
-
-    requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
 }
 
 animate();
